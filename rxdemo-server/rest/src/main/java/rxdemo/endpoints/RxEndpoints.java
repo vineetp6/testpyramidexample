935022a3f23067a27b3141ae8db6737cd5b4ccf9
vineetp6 <vineetp6@gmail.com>
2021-12-22 17:16:33 -0800

    Update RxEndpoints.java

--- rxdemo-server/rest/src/main/java/rxdemo/endpoints/RxEndpoints.java
+++ rxdemo-server/rest/src/main/java/rxdemo/endpoints/RxEndpoints.java
@@ -11,7 +11,7 @@ import spark.Response;
 import static spark.Spark.*;
 
 // change to be deployed
-// Trigger build
+// Trigger build new
 
 public class RxEndpoints {
    public static void initalizeEndpoints() {
